bafb6a3f8c88efc3824f2b921d3faa08e15f1aef
Liz Portnoy <lizport10@gmail.com>
2014-12-11 13:39:08 -0800

    began payments

--- server/lib/routehandlers.js
+++ server/lib/routehandlers.js
@@ -30,7 +30,10 @@ exports.logout = function (req, res) {
       req.logout();
       res.send('loggedOut')
   });
+<<<<<<< HEAD
 
+=======
+>>>>>>> began payments
 };
 
 exports.getGoals = function (req, response) {
